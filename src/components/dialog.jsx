1317f7ce604ab4d8034128e573b4774b85b6f5f1
Hajar <hajar.kattaa@net.usj.edu.lb>
2024-03-28 00:11:38 +0200

    added patient info form

--- src/components/dialog.jsx
+++ src/components/dialog.jsx
@@ -18,7 +18,6 @@ function DialogForm() {
         <p>Drag n drop some files here, or click to select files</p>
       </div>
       <aside>
-        <h4>Files</h4>
         <ul>{files}</ul>
       </aside>
     </section>
--- src/components/uploadButton.jsx
+++ src/components/uploadButton.jsx
@@ -6,17 +6,48 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import AddIcon from '@mui/icons-material/Add';
-import { TextField } from '@mui/material';
+import { Checkbox, FormControlLabel, TextField } from '@mui/material';
 import DialogForm from './dialog';
 
 export default function UploadButton() {
   const [open, setOpen] = React.useState(false);
+  const [patientInfo, setPatientInfo] = React.useState({
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+  });
+  const [agreed, setAgreed] = React.useState(false);
 
   const handleClick = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
+    setOpen(false);
+  };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPatientInfo((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleCheckboxChange = (event) => {
+    setAgreed(event.target.checked);
+  };
+
+  const handleSubmit = () => {
+    // Handle form submission here
+    console.log(patientInfo);
+    console.log('Agreed to use picture:', agreed);
+    // Reset form fields
+    setPatientInfo({
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+    });
+    setAgreed(false);
     setOpen(false);
   };
 
@@ -27,15 +58,51 @@ export default function UploadButton() {
           New{' '}
         </Button>
         <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Upload Files</DialogTitle>
+          <DialogTitle>Patient Info</DialogTitle>
           <DialogContent>
-            {/* Render the MyDropzone component here */}
-            {/* Include other components here */}
-            <TextField label="Insert Your Name" variant="outlined" />
+            <TextField
+              margin="dense"
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              type="text"
+              fullWidth
+              value={patientInfo.firstName}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              type="text"
+              fullWidth
+              value={patientInfo.lastName}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              id="phoneNumber"
+              name="phoneNumber"
+              label="Phone Number"
+              type="tel" // Changed type to "tel" for phone number input
+              fullWidth
+              value={patientInfo.phoneNumber}
+              onChange={handleInputChange}
+              InputProps={{
+                inputMode: 'tel', // Added inputMode attribute
+              }}
+            />
+
             <DialogForm />
+            <FormControlLabel
+              control={<Checkbox checked={agreed} onChange={handleCheckboxChange} />}
+              label="Does the patient agree to use his/her picture in our data?"
+            />
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose}>Close</Button>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleSubmit}>Submit</Button>
           </DialogActions>
         </Dialog>
       </div>
